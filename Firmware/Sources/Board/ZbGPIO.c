32839a43d55ceca2134de1515ac78df747b61f11
Mgainullin <m.gainullin@proton-electrotex.com>
2021-06-28 11:53:51 +0300

    Инициализация портов практически без изменений

--- Firmware/Sources/Board/ZbGPIO.c
+++ Firmware/Sources/Board/ZbGPIO.c
@@ -18,49 +18,47 @@ static const Int16U EmptyPins[] = { PIN_EMPTY_1, PIN_EMPTY_2, PIN_EMPTY_3, PIN_E
 //
 void ZbGPIO_Init()
 {
-	Int16U i;
+    Int16U i;
 
-	// Hold down empty DSC pins
-	for(i = 0; i < sizeof(EmptyPins) / sizeof(Int16U); ++i)
-	{
-		ZwGPIO_WritePin(EmptyPins[i], FALSE);
-	   	ZwGPIO_PinToOutput(EmptyPins[i]);
-	}
+    // Hold down empty DSC pins
+    for(i = 0; i < sizeof(EmptyPins) / sizeof(Int16U); ++i)
+    {
+        ZwGPIO_WritePin(EmptyPins[i], FALSE);
+        ZwGPIO_PinToOutput(EmptyPins[i]);
+    }
 
-	// Configure input pin
-	ZwGPIO_PinToInput(PIN_SYNC_IN, FALSE, GPIO_NSAMPLE);
-	ZwGPIO_PinToInput(PIN_DETECTOR, FALSE, GPIO_NSAMPLE);
+    // Configure input pin
+    ZwGPIO_PinToInput(PIN_SYNC_IN, FALSE, GPIO_NSAMPLE);
+    ZwGPIO_PinToInput(PIN_DETECTOR, FALSE, GPIO_NSAMPLE);
 
-	// Reset to default state
-	ZwGPIO_WritePin(PIN_LED_1, FALSE);
-	ZwGPIO_WritePin(PIN_LED_2, FALSE);
-	ZwGPIO_WritePin(PIN_DEBUG, FALSE);
-	ZwGPIO_WritePin(PIN_RESULT_OUT, FALSE);
-	ZwGPIO_WritePin(PIN_START_PULSE, FALSE);
-	ZwGPIO_WritePin(PIN_SYNC_EN, INVERT_SYNC ? TRUE : FALSE);
-	ZwGPIO_WritePin(PIN_OPT_V1V2, FALSE);
-	ZwGPIO_WritePin(PIN_OPT_V3V4, FALSE);
-	ZwGPIO_WritePin(PIN_OPT_V5V6, FALSE);
-	ZwGPIO_WritePin(PIN_OPT_V7V8, FALSE);
-	ZwGPIO_WritePin(PIN_CELL_IND_1, FALSE);
-	ZwGPIO_WritePin(PIN_CELL_IND_2, FALSE);
-	ZwGPIO_WritePin(PIN_CELL_IND_3, FALSE);
-	ZwGPIO_WritePin(PIN_MEANWELL_EN, FALSE);
-   	// Configure pins
-   	ZwGPIO_PinToOutput(PIN_LED_1);
-   	ZwGPIO_PinToOutput(PIN_LED_2);
-   	ZwGPIO_PinToOutput(PIN_DEBUG);
-   	ZwGPIO_PinToOutput(PIN_RESULT_OUT);
-   	ZwGPIO_PinToOutput(PIN_START_PULSE);
-   	ZwGPIO_PinToOutput(PIN_SYNC_EN);
-   	ZwGPIO_PinToOutput(PIN_OPT_V1V2);
-   	ZwGPIO_PinToOutput(PIN_OPT_V3V4);
-   	ZwGPIO_PinToOutput(PIN_OPT_V5V6);
-   	ZwGPIO_PinToOutput(PIN_OPT_V7V8);
-   	ZwGPIO_PinToOutput(PIN_CELL_IND_1);
-   	ZwGPIO_PinToOutput(PIN_CELL_IND_2);
-   	ZwGPIO_PinToOutput(PIN_CELL_IND_3);
-   	ZwGPIO_PinToOutput(PIN_MEANWELL_EN);
+    // Reset to default state
+    ZwGPIO_WritePin(PIN_LED_1, FALSE);
+    ZwGPIO_WritePin(PIN_LED_2, FALSE);
+    ZwGPIO_WritePin(PIN_DEBUG, FALSE);
+    ZwGPIO_WritePin(PIN_RESULT_OUT, FALSE);
+    ZwGPIO_WritePin(PIN_START_PULSE, TRUE);
+    ZwGPIO_WritePin(PIN_SYNC_EN, TRUE);
+    ZwGPIO_WritePin(PIN_OPT_V3V4, FALSE);
+    ZwGPIO_WritePin(PIN_OPT_V5V6, FALSE);
+    ZwGPIO_WritePin(PIN_OPT_V7V8, FALSE);
+    ZwGPIO_WritePin(PIN_CELL_IND_1, FALSE);
+    ZwGPIO_WritePin(PIN_CELL_IND_2, FALSE);
+    ZwGPIO_WritePin(PIN_CELL_IND_3, FALSE);
+    ZwGPIO_WritePin(PIN_MEANWELL_EN, FALSE);
+    // Configure pins
+    ZwGPIO_PinToOutput(PIN_LED_1);
+    ZwGPIO_PinToOutput(PIN_LED_2);
+    ZwGPIO_PinToOutput(PIN_DEBUG);
+    ZwGPIO_PinToOutput(PIN_RESULT_OUT);
+    ZwGPIO_PinToOutput(PIN_START_PULSE);
+    ZwGPIO_PinToOutput(PIN_SYNC_EN);
+    ZwGPIO_PinToOutput(PIN_OPT_V3V4);
+    ZwGPIO_PinToOutput(PIN_OPT_V5V6);
+    ZwGPIO_PinToOutput(PIN_OPT_V7V8);
+    ZwGPIO_PinToOutput(PIN_CELL_IND_1);
+    ZwGPIO_PinToOutput(PIN_CELL_IND_2);
+    ZwGPIO_PinToOutput(PIN_CELL_IND_3);
+    ZwGPIO_PinToOutput(PIN_MEANWELL_EN);
 }
 // ----------------------------------------
 
@@ -72,66 +70,66 @@ void ZbGPIO_SwitchFAN(Boolean Set)
 
 void ZbGPIO_SwitchLED1(Boolean Set)
 {
-	ZwGPIO_WritePin(PIN_LED_1, Set);
+    ZwGPIO_WritePin(PIN_LED_1, Set);
 }
 // ----------------------------------------
 
 void ZbGPIO_SwitchLED2(Boolean Set)
 {
-	ZwGPIO_WritePin(PIN_LED_2, Set);
+    ZwGPIO_WritePin(PIN_LED_2, Set);
 }
 // ----------------------------------------
 
 void ZbGPIO_SwitchLEDExt(Boolean Set)
 {
-	ZwGPIO_WritePin(PIN_OPT_V7V8, Set);
+    ZwGPIO_WritePin(PIN_OPT_V7V8, Set);
 }
 // ----------------------------------------
 
 void ZbGPIO_ToggleLED1()
 {
-	ZwGPIO_TogglePin(PIN_LED_1);
+    ZwGPIO_TogglePin(PIN_LED_1);
 }
 // ----------------------------------------
 
 void ZbGPIO_ToggleLED2()
 {
-	ZwGPIO_TogglePin(PIN_LED_2);
+    ZwGPIO_TogglePin(PIN_LED_2);
 }
 // ----------------------------------------
 
 void ZbGPIO_SwitchMeanwell(Boolean Enable)
 {
-	ZwGPIO_WritePin(PIN_MEANWELL_EN, Enable);
+    ZwGPIO_WritePin(PIN_MEANWELL_EN, Enable);
 }
 // ----------------------------------------
 
 void ZbGPIO_SwitchResultOut(Boolean Set)
 {
-	ZwGPIO_WritePin(PIN_RESULT_OUT, Set);
+    ZwGPIO_WritePin(PIN_RESULT_OUT, Set);
 }
 // ----------------------------------------
 
 void ZbGPIO_SwitchSyncEn(Boolean Enable)
 {
-	ZwGPIO_WritePin(PIN_SYNC_EN, INVERT_SYNC ? (!Enable) : Enable);
+    ZwGPIO_WritePin(PIN_SYNC_EN, !Enable);
 }
 // ----------------------------------------
 
 void ZbGPIO_RelayLine(Boolean Out)
 {
-	ZwGPIO_WritePin(PIN_OPT_V3V4, Out);
-	ZwGPIO_WritePin(PIN_OPT_V5V6, Out);
+    ZwGPIO_WritePin(PIN_OPT_V3V4, Out);
+    ZwGPIO_WritePin(PIN_OPT_V5V6, Out);
 }
 // ----------------------------------------
 
 void ZbGPIO_SetActiveCell(Int16U CellNumber)
 {
-	ZwGPIO_WritePin(PIN_CELL_IND_1, CellNumber & 0x01);
-	ZwGPIO_WritePin(PIN_CELL_IND_2, (CellNumber >> 1) & 0x01);
-	ZwGPIO_WritePin(PIN_CELL_IND_3, (CellNumber >> 2) & 0x01);
+    ZwGPIO_WritePin(PIN_CELL_IND_1, CellNumber & 0x01);
+    ZwGPIO_WritePin(PIN_CELL_IND_2, (CellNumber >> 1) & 0x01);
+    ZwGPIO_WritePin(PIN_CELL_IND_3, (CellNumber >> 2) & 0x01);
 
-	DELAY_US(1);
+    DELAY_US(1);
 }
 // ----------------------------------------
 
