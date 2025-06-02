31874eb5631dfd5a580f6355c749ea3aefca0555
IgnatevA1 <a.ignatyev@proton-electrotex.com>
2025-06-02 19:28:48 +0300

    Добавление значения режима Qrr tq в Constraints

--- Firmware/Sources/Platform/Constraints.c
+++ Firmware/Sources/Platform/Constraints.c
@@ -35,7 +35,7 @@ const TableItemConstraint NVConstraint[DATA_TABLE_NV_SIZE] =
 										{NO, YES, YES},															// 19
 										{0, 0, 0},																// 20
 										{0, INT16U_MAX, 0},														// 21
-										{0, 0, 0},																// 22
+										{NO, YES, NO},															// 22
 										{0, 0, 0},																// 23
 										{0, 0, 0},																// 24
 										{0, 0, 0},																// 25
